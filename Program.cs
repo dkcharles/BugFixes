e39ce3bb6ce41becc054ff4169da23940c253f17
Darryl Charles <dk.charles@ulster.ac.uk>
2024-10-25 12:04:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alice";
+            if (name == 'Alice') // 
+            {
+                Console.WriteLine("Hello, Alice!");
+            }
+        }
+    }
+}
+
+/*
+Quiz Question 3: What is the problem with the above code?
+
+A. Strings must always be compared using "Equals" method.
+B. Strings in C# must be enclosed in double quotes.
+C. Single quotes are for strings only.
+D. "name" is not declared properly.
+
+*/
